18c9b6eb25af84bf0ba126c33f2e0b5f28f24b16
Firmino Changani <firmino.changani@gmail.com>
2021-09-02 23:28:00 +0100

    fix: add branches config

---
+++ release.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    [
+      "@semantic-release/gitlab",
+      {
+        gitlabUrl: "https://gitlab.com",
+        gitlabApiPathPrefix: "/api/v4",
+      },
+    ],
+  ],
+  repositoryUrl: "https://gitlab.com/firmino.changani/cryptofeed-react",
+};
